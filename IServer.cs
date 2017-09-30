1ed3cf1dd2d1114de6a8e7eca7b1d13acc0ec4f1
Yonatan435 <32424107+Yonatan435@users.noreply.github.com>
2017-10-01 00:27:04 +0300

    Initial commit

---
+++ IServer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    [ServiceContract(Name = "Server_Service"/*, Namespace = "GPH_QuickMessageServiceLib"*/, SessionMode = SessionMode.Required, CallbackContract = typeof(IServerCallback))]
+
+    interface IServer
+    {
+        [OperationContract]
+        bool Authenticate(string userName, string password);
+        [OperationContract]
+        void StartDataTransfer();
+        [OperationContract]
+        void StopDataTransfer();
+    }
+    interface IServerCallback
+    {
+        [OperationContract]
+        void SendNumberToClient(int number);
+    }
+}
