79dda7cc2869c488adee2300cbffa3445ffdefd5
Yonatan435 <32424107+Yonatan435@users.noreply.github.com>
2017-10-03 00:28:26 +0300

    Add files via upload

---
+++ Exercise/Server/Service.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Service : IServer
+    {
+        List<KeyValuePair<string, string>> users;
+        bool _sendData;
+        public Service()
+        {
+            users.Add(new KeyValuePair<string, string>("TestUser", "TestPassword"));
+        }
+        bool Authenticate(string userName, string password)
+        {
+            bool userExists = users.Find(x=> x.Key == userName && x.Value == password).Equals(new KeyValuePair<string, string>());
+            return userExists;
+        }
+        void StartDataTransfer()
+        {
+            Random rnd = new Random();
+            _sendData = true;
+            byte[] bytes = new byte[1000];
+            while (_sendData)
+            {
+                rnd.NextBytes(bytes);
+            }
+        }
+        void StopDataTransfer()
+        {
+            _sendData = false;
+        }
+
+    }
+}
